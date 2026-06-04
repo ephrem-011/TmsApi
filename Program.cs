6eeaafbf1f6e79e03e1117af9ef33ee4a59b515c
ephrem-011 <ephraimfekadu@gmail.com>
2026-06-04 08:17:15 -0700

    fix: Resolved middleware order and implemented authentication

---
+++ Program.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication();
+
+var app = builder.Build();
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapGet("/api/assessments/results", () => Results.Ok(new
+{
+courseCode = "CS-101", studentId = "S-001",
+letterGrade = "A"
+}));
+
+app.Run();
